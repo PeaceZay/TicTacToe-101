f7790becf2ebef7a7b4db3978b63277ee89328c7
PeaceZay <zay.smith007@gmail.com>
2022-02-06 12:52:15 -0600

    completed 2/5

--- scripts.js
+++ scripts.js
@@ -8,6 +8,11 @@
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
+let board =   
+[["","",""],
+["","",""],
+["","",""]];
+
 
 
 
@@ -15,7 +20,7 @@ let currentMarker = 'X'
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
-
+  
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
 
@@ -42,6 +47,7 @@ const handleClick = (element) => {
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
+ 
 
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
@@ -59,12 +65,17 @@ document.getElementById(id).innerHTML = currentMarker
 
 
 
-
-  changeMarker()
+  updateBoard(id)
+  checkForWin()
 }
 
 
-
+const updateBoard = (id) => {
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+  console.log(`You Clicked Square with row ${row} and column ${column}`)
+  board[row][column] = currentMarker
+}
 
 
 
@@ -83,7 +94,53 @@ const changeMarker = () => {
 
 
 
+const checkForWin = () => {
+  if (horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+    currentMarker = "X"
+    board =
+      [["", "", ""],
+      ["", "", ""],
+      ["", "", ""]];
+  } else {
+    console.log("Check for win False")
+    changeMarker()
+  }
+}
 
+const horizontalWin = () => {
+  // Your code here to check for horizontal wins
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+    || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+    || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X")
+    || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+    || (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X")
+    || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) {
+    return true;
+  }
+}
+
+const verticalWin = () => {
+  // Your code here to check for vertical wins
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+  || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
+  || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
+  || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X")
+  || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X")) {
+    return true;
+  }
+}
+
+const diagonalWin = () => {
+  // Your code here to check for diagonal wins
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  || (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")) {
+return true;
+  }
+}
 
 
 
